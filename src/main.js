6beef229d4bc91a3c6e9699300776bc22e01d094
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-06-10 19:06:53 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ const WIDTH = 1000,
 
 const landerWidth = 30;
 const footWidth = 0.15; //* landerWidth
-const landerRotAngle = 0.01; // radians per frame
-const landerBoosterStrength = 0.000375 //* lander mass
+const landerRotAngle = 0.015; // radians per frame
+const landerBoosterStrength = 0.000275 //* lander mass
 //const landerBoosterStrength = 1;
 const moonGravity = 0.35;
 
@@ -58,32 +58,32 @@ function draw() {
 
     //timeStep(true);
 
-    //global framecount
-    //if (frameCount > 5000){
+    // //global framecount
+    // if (frameCount > 5000){
     //  timeStep(false);
-    //}else{
+    // }else{
     //  for (let i = 0; i < 100; i ++){
     //    timeStep(true);
-    //}
-    //}
+    // }
+    // }
 
-    // let left = 0
-    // let right = 0
-    // let up = 0
-    // //TEMP TESTING
-    // if (keyIsDown(LEFT_ARROW)){
-    //     left = 1
-    // }
-    // if (keyIsDown(RIGHT_ARROW)){
-    //     right = 1
-    // }
-    // if (keyIsDown(UP_ARROW)){
-    //     up = 1
-    // }
-    // if (keyIsDown(DOWN_ARROW)){
-    //     noLoop();
-    // }
-    // m.landers[0].Move([left, right, up]);
+    let left = 0
+    let right = 0
+    let up = 0
+    //TEMP TESTING
+    if (keyIsDown(LEFT_ARROW)){
+        left = 1
+    }
+    if (keyIsDown(RIGHT_ARROW)){
+        right = 1
+    }
+    if (keyIsDown(UP_ARROW)){
+        up = 1
+    }
+    if (keyIsDown(DOWN_ARROW)){
+        noLoop();
+    }
+    m.landers[0].Move([left, right, up]);
 }
 
 function timeStep(pb = true) {
